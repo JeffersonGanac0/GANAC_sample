fdb775e29593248b374b49e8104b962206784260
JeffersonGanac0 <ganacjefferson@gmail.com>
2024-09-24 09:02:13 +0800

    first upload

---
+++ hello.py
@@ -0,0 +1,17 @@
+#this command is for displaying output
+print("Hello World")
+
+first_name="Jefferson"
+last_name="Ganac"
+
+print(first_name)
+print(last_name)
+print(first_name, last_name)
+print(first_name + last_name)
+print (first_name + " " + last_name)
+
+x = "awesome"
+print("python is " + x)
+
+y = "good"
+print(y)
---
+++ laboratory2.py
@@ -0,0 +1,13 @@
+first_name = "Jefferson"
+last_name = "Ganac"
+age = str(18)
+
+full_name =  first_name + " " + last_name
+
+print("Hello, my name is " + first_name + " " + last_name + ". I am " + age + " Years Old ")
+
+if (full_name.upper == True):
+    print(full_name)
+else:
+    new_full = full_name.upper()
+    print(new_full)
